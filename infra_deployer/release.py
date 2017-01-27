8090017b13c77bd18a272428aa89efc8f71f0799
Tom Yandell <tom@tomyandell.co>
2017-01-27 15:41:32 +0000

    PLAT-764 - Log docker into ECR
    So that we can push docker images to ECR.

--- infra_deployer/release.py
+++ infra_deployer/release.py
@@ -2,6 +2,8 @@ from subprocess import check_call
 
 from collections import namedtuple
 
+from base64 import b64decode
+
 
 ReleaseConfig = namedtuple('ReleaseConfig', [
     'dev_account_id',
@@ -11,9 +13,10 @@ ReleaseConfig = namedtuple('ReleaseConfig', [
 
 class Release(object):
 
-    def __init__(self, config, component_name, version='dev'):
+    def __init__(self, config, boto_ecr_client, component_name, version=None):
         self._dev_account_id = config.dev_account_id
         self._aws_region = config.aws_region
+        self._boto_ecr_client = boto_ecr_client
         self._component_name = component_name
         self._version = version
 
@@ -22,7 +25,30 @@ class Release(object):
             self._dev_account_id,
             self._aws_region,
             self._component_name,
-            self._version
+            self._version or 'dev'
         )
 
         check_call(['docker', 'build', '-t', image_name])
+
+        if self._version:
+            self._ensure_ecr_repo_exists()
+            self._docker_login()
+            # docker push
+
+    def _ensure_ecr_repo_exists(self):
+        self._boto_ecr_client.describe_repositories(
+            repositoryNames=[self._component_name]
+        )
+
+    def _docker_login(self):
+        response = self._boto_ecr_client.get_authorization_details()
+
+        username, password = b64decode(
+            response['authorizationData'][0]['authorizationToken']
+        ).split(':')
+
+        proxy_endpoint = response['authorizationData'][0]['proxyEndpoint']
+
+        check_call(
+            ['docker', 'login', '-u', username, '-p', password, proxy_endpoint]
+        )
--- test/test_release.py
+++ test/test_release.py
@@ -1,8 +1,9 @@
 import unittest
 
 from string import uppercase, lowercase, digits
+from base64 import b64encode
 
-from mock import patch
+from mock import patch, Mock
 from hypothesis import given, settings
 from hypothesis.strategies import text
 
@@ -22,7 +23,8 @@ class TestRelease(unittest.TestCase):
         self, component_name, dev_account_id, aws_region
     ):
         config = ReleaseConfig(dev_account_id, aws_region)
-        release = Release(config, component_name)
+        boto_ecr_client = Mock()
+        release = Release(config, boto_ecr_client, component_name)
         with patch('infra_deployer.release.check_call') as check_call:
             release.create()
 
@@ -44,7 +46,8 @@ class TestRelease(unittest.TestCase):
         aws_region = 'dummy-region'
 
         config = ReleaseConfig(dev_account_id, aws_region)
-        release = Release(config, component_name, version)
+        boto_ecr_client = Mock()
+        release = Release(config, boto_ecr_client, component_name, version)
 
         with patch('infra_deployer.release.check_call') as check_call:
             release.create()
@@ -59,3 +62,44 @@ class TestRelease(unittest.TestCase):
             check_call.assert_called_once_with(
                 ['docker', 'build', '-t', image_name]
             )
+
+    @given(text(alphabet=IDENTIFIER_ALPHABET + ':/', min_size=8, max_size=16))
+    @given(text(alphabet=IDENTIFIER_ALPHABET, min_size=8, max_size=16))
+    @given(text(alphabet=IDENTIFIER_ALPHABET, min_size=8, max_size=16))
+    @settings(max_examples=10)
+    def test_build_with_version_pushes_to_repo(
+        self, proxy_endpoint, username, password
+    ):
+        dev_account_id = 'dummy-account-id'
+        aws_region = 'dummy-region'
+        component_name = 'dummy-component'
+        version = '1.2.3'
+
+        boto_ecr_client = Mock()
+        boto_ecr_client.get_authorization_details.return_value = {
+            'authorizationData': [
+                {
+                    'authorizationToken': b64encode('{}:{}'.format(
+                        username, password
+                    )),
+                    'proxyEndpoint': proxy_endpoint
+                }
+            ]
+        }
+
+        config = ReleaseConfig(dev_account_id, aws_region)
+        release = Release(config, boto_ecr_client, component_name, version)
+
+        with patch('infra_deployer.release.check_call') as check_call:
+            release.create()
+
+            boto_ecr_client.describe_repositories.assert_called_once_with(
+                repositoryNames=[component_name]
+            )
+
+            boto_ecr_client.get_authorization_details.assert_called_once()
+
+            check_call.assert_any_call([
+                'docker', 'login',
+                '-u', username, '-p', password, proxy_endpoint
+            ])
