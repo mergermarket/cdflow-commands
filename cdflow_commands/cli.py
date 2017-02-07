3aa7dffa4b368ba6ea3a83448955d4af4e44f853
Tom Yandell <tom@tomyandell.co>
2017-02-07 11:52:42 +0000

    PLAT-764 - Integrate deploy into cli
    Make it so that we can deploy with the CI - so far in dev environments
    only.

--- cdflow_commands/cli.py
+++ cdflow_commands/cli.py
@@ -1,15 +1,15 @@
-'''
-CDFlow Commands
+"""CDFlow Commands.
 
 Commands for managing the software lifecycle.
 
 Usage:
     cdflow-commands release [<version>] [options]
+    cdflow-commands deploy <environment> <version> [options]
 
 Options:
     -c <component_name>, --component <component_name>
 
-'''
+"""
 import os
 
 from boto3.session import Session
@@ -23,32 +23,65 @@ from cdflow_commands.config import (
     get_component_name
 )
 from cdflow_commands.release import Release, ReleaseConfig
+from cdflow_commands.deploy import Deploy, DeployConfig
 
 
 def run(argv):
     args = docopt(__doc__, argv=argv)
 
+    metadata = load_service_metadata()
+    global_config = load_global_config(
+        metadata.account_prefix, metadata.aws_region
+    )
+    root_session = Session(region_name=metadata.aws_region)
+    component_name = get_component_name(args['--component'])
+
     if args['release']:
-        metadata = load_service_metadata()
-        global_config = load_global_config(
-            metadata.account_prefix, metadata.aws_region
+        run_release(
+            args, metadata, global_config, root_session, component_name
         )
-        root_session = Session(region_name=metadata.aws_region)
-        boto_session = assume_role(
-            root_session,
-            global_config.dev_account_id,
-            get_role_session_name(os.environ)
-        )
-        ecr_client = boto_session.client('ecr')
-        release_config = ReleaseConfig(
-            global_config.dev_account_id,
-            global_config.prod_account_id,
-            metadata.aws_region
+    elif args['deploy']:
+        run_deploy(
+            args, metadata, global_config, root_session, component_name
         )
 
-        component_name = get_component_name(args['--component'])
 
-        release = Release(
-            release_config, ecr_client, component_name, args['<version>']
-        )
-        release.create()
+def run_release(args, metadata, global_config, root_session, component_name):
+    boto_session = assume_role(
+        root_session,
+        global_config.dev_account_id,
+        get_role_session_name(os.environ)
+    )
+    ecr_client = boto_session.client('ecr')
+    release_config = ReleaseConfig(
+        global_config.dev_account_id,
+        global_config.prod_account_id,
+        metadata.aws_region
+    )
+
+    release = Release(
+        release_config, ecr_client, component_name, args['<version>']
+    )
+    release.create()
+
+
+def run_deploy(args, metadata, global_config, root_session, component_name):
+
+    platform_config_file = './infra/platform-config/{}/{}/{}.json'.format(
+        metadata.account_prefix, 'dev', metadata.aws_region
+    )
+    deploy_config = DeployConfig(
+        team=metadata.team,
+        account_id=global_config.dev_account_id,
+        platform_config_file=platform_config_file,
+    )
+    boto_session = assume_role(
+        root_session,
+        global_config.dev_account_id,
+        get_role_session_name(os.environ)
+    )
+    deployment = Deploy(
+        boto_session, component_name, args['<environment>'], args['<version>'],
+        deploy_config
+    )
+    deployment.run()
--- test/test_cli.py
+++ test/test_cli.py
@@ -3,6 +3,7 @@ import json
 import unittest
 
 from datetime import datetime
+from collections import namedtuple
 
 from mock import patch, Mock, mock_open, MagicMock
 
@@ -17,7 +18,8 @@ class TestReleaseCLI(unittest.TestCase):
     @patch('cdflow_commands.config.open', new_callable=mock_open, create=True)
     @patch('cdflow_commands.release.check_call')
     def test_release_is_configured_and_created(
-        self, check_call, mock_open, configSession, cliSession, mock_os
+        self, check_call, mock_open,
+        Session_from_config, Session_from_cli, mock_os
     ):
         mock_metadata_file = MagicMock(spec=file)
         metadata = {
@@ -50,7 +52,7 @@ class TestReleaseCLI(unittest.TestCase):
 
         mock_root_session = Mock()
         mock_root_session.region_name = 'eu-west-12'
-        cliSession.return_value = mock_root_session
+        Session_from_cli.return_value = mock_root_session
 
         mock_ecr_client = Mock()
         mock_ecr_client.get_authorization_token.return_value = {
@@ -64,7 +66,7 @@ class TestReleaseCLI(unittest.TestCase):
 
         mock_session = Mock()
         mock_session.client.return_value = mock_ecr_client
-        configSession.return_value = mock_session
+        Session_from_config.return_value = mock_session
 
         mock_sts = Mock()
         mock_sts.assume_role.return_value = {
@@ -98,7 +100,7 @@ class TestReleaseCLI(unittest.TestCase):
         )
 
         check_call.assert_any_call(['docker', 'build', '-t', image_name, '.'])
-        check_call.assert_any_call(['docker', 'push',  image_name])
+        check_call.assert_any_call(['docker', 'push', image_name])
 
     @patch('cdflow_commands.cli.os')
     @patch('cdflow_commands.cli.Session')
@@ -108,7 +110,7 @@ class TestReleaseCLI(unittest.TestCase):
     @patch('cdflow_commands.release.check_call')
     def test_release_uses_component_name_from_origin(
         self, check_call, check_output, mock_open,
-        configSession, cliSession, mock_os
+        Session_from_config, Session_from_cli, mock_os
     ):
         mock_metadata_file = MagicMock(spec=file)
         metadata = {
@@ -141,7 +143,7 @@ class TestReleaseCLI(unittest.TestCase):
 
         mock_root_session = Mock()
         mock_root_session.region_name = 'eu-west-12'
-        cliSession.return_value = mock_root_session
+        Session_from_cli.return_value = mock_root_session
 
         mock_ecr_client = Mock()
         mock_ecr_client.get_authorization_token.return_value = {
@@ -155,7 +157,7 @@ class TestReleaseCLI(unittest.TestCase):
 
         mock_session = Mock()
         mock_session.client.return_value = mock_ecr_client
-        configSession.return_value = mock_session
+        Session_from_config.return_value = mock_session
 
         mock_sts = Mock()
         mock_sts.assume_role.return_value = {
@@ -194,4 +196,131 @@ class TestReleaseCLI(unittest.TestCase):
         )
 
         check_call.assert_any_call(['docker', 'build', '-t', image_name, '.'])
-        check_call.assert_any_call(['docker', 'push',  image_name])
+        check_call.assert_any_call(['docker', 'push', image_name])
+
+
+BotoCreds = namedtuple('BotoCreds', ['access_key', 'secret_key', 'token'])
+
+
+class TestDeployCLI(unittest.TestCase):
+
+    @patch('cdflow_commands.cli.os')
+    @patch('cdflow_commands.cli.Session')
+    @patch('cdflow_commands.config.Session')
+    @patch('cdflow_commands.config.open', new_callable=mock_open, create=True)
+    @patch('cdflow_commands.deploy.check_call')
+    @patch('cdflow_commands.config.check_output')
+    def test_deploy_is_configured_and_run(
+        self, check_output, check_call, mock_open,
+        Session_from_config, Session_from_cli, mock_os
+    ):
+        mock_os.environ = {
+            'JOB_NAME': 'dummy-job-name'
+        }
+
+        mock_metadata_file = MagicMock(spec=file)
+        metadata = {
+            'TEAM': 'dummy-team',
+            'TYPE': 'docker',
+            'REGION': 'eu-west-12',
+            'ACCOUNT_PREFIX': 'mmg'
+        }
+        mock_metadata_file.read.return_value = json.dumps(metadata)
+
+        mock_dev_file = MagicMock(spec=file)
+        dev_config = {
+            'platform_config': {
+                'account_id': 123456789
+            }
+        }
+        mock_dev_file.read.return_value = json.dumps(dev_config)
+
+        mock_prod_file = MagicMock(spec=file)
+        prod_config = {
+            'platform_config': {
+                'account_id': 987654321
+            }
+        }
+        mock_prod_file.read.return_value = json.dumps(prod_config)
+
+        mock_open.return_value.__enter__.side_effect = (
+            f for f in (mock_metadata_file, mock_dev_file, mock_prod_file)
+        )
+
+        mock_sts_client = Mock()
+        mock_sts_client.assume_role.return_value = {
+            'Credentials': {
+                'AccessKeyId': 'dummy-access-key',
+                'SecretAccessKey': 'dummy-secret-key',
+                'SessionToken': 'dummy-session-token',
+            }
+        }
+
+        mock_root_session = Mock()
+        mock_root_session.client.return_value = mock_sts_client
+        mock_root_session.region_name = 'eu-west-12'
+        Session_from_cli.return_value = mock_root_session
+
+        aws_access_key_id = 'dummy-access-key-id'
+        aws_secret_access_key = 'dummy-secret-access-key'
+        aws_session_token = 'dummy-session-token'
+        mock_assumed_session = Mock()
+        mock_assumed_session.region_name = 'eu-west-12'
+        mock_assumed_session.get_credentials.return_value = BotoCreds(
+            aws_access_key_id,
+            aws_secret_access_key,
+            aws_session_token
+        )
+
+        Session_from_config.return_value = mock_assumed_session
+
+        component_name = 'dummy-component'
+
+        check_output.return_value = 'git@github.com:org/{}.git'.format(
+            component_name
+        )
+
+        cli.run(['deploy', 'aslive', '1.2.3'])
+
+        check_call.assert_any_call(['terraform', 'get', 'infra'])
+
+        image_name = (
+            '123456789.dkr.ecr.eu-west-12.amazonaws.com/'
+            'dummy-component:1.2.3'
+        )
+
+        check_call.assert_any_call(
+            [
+                'terragrunt', 'plan', 'infra',
+                '-var', 'component=dummy-component',
+                '-var', 'aws_region=eu-west-12',
+                '-var', 'env=aslive',
+                '-var', 'image={}'.format(image_name),
+                '-var', 'team=dummy-team',
+                '-var', 'version=1.2.3',
+                '-var-file', './infra/platform-config/mmg/dev/eu-west-12.json'
+            ],
+            env={
+                'AWS_ACCESS_KEY_ID': aws_access_key_id,
+                'AWS_SECRET_ACCESS_KEY': aws_secret_access_key,
+                'AWS_SESSION_TOKEN': aws_session_token
+            }
+        )
+
+        check_call.assert_any_call(
+            [
+                'terragrunt', 'apply', 'infra',
+                '-var', 'component=dummy-component',
+                '-var', 'aws_region=eu-west-12',
+                '-var', 'env=aslive',
+                '-var', 'image={}'.format(image_name),
+                '-var', 'team=dummy-team',
+                '-var', 'version=1.2.3',
+                '-var-file', './infra/platform-config/mmg/dev/eu-west-12.json'
+            ],
+            env={
+                'AWS_ACCESS_KEY_ID': aws_access_key_id,
+                'AWS_SECRET_ACCESS_KEY': aws_secret_access_key,
+                'AWS_SESSION_TOKEN': aws_session_token
+            }
+        )
