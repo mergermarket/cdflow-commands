054f2ebe48e259d6ed133af55fe0f779be258aa5
grahamlyons <graham@grahamlyons.com>
2017-02-17 16:59:31 +0000

    Update Dockerfile to Python 3 on Debian Jesse
    Install the required packages and upgrade Terraform and Terragrunt

--- test/test_config.py
+++ test/test_config.py
@@ -246,8 +246,10 @@ class TestGetComponentName(unittest.TestCase):
     ))
     def test_component_not_passed_as_argument(self, component_name):
         with patch('cdflow_commands.config.check_output') as check_output:
-            check_output.return_value = 'git@github.com:org/{}.git\n'.format(
-                component_name
+            check_output.return_value = bytes(
+                'git@github.com:org/{}.git\n'.format(
+                    component_name
+                ).encode('utf-8')
             )
             extraced_component_name = config.get_component_name(None)
 
@@ -260,8 +262,10 @@ class TestGetComponentName(unittest.TestCase):
         self, component_name
     ):
         with patch('cdflow_commands.config.check_output') as check_output:
-            check_output.return_value = 'git@github.com:org/{}\n'.format(
-                component_name
+            check_output.return_value = bytes(
+                'git@github.com:org/{}\n'.format(
+                    component_name
+                ).encode('utf-8')
             )
             extraced_component_name = config.get_component_name(None)
 
@@ -275,8 +279,8 @@ class TestGetComponentName(unittest.TestCase):
     ):
         with patch('cdflow_commands.config.check_output') as check_output:
             repo_template = 'https://github.com/org/{}.git\n'
-            check_output.return_value = repo_template.format(
-                component_name
+            check_output.return_value = bytes(
+                repo_template.format(component_name).encode('utf-8')
             )
             extraced_component_name = config.get_component_name(None)
 
@@ -289,8 +293,10 @@ class TestGetComponentName(unittest.TestCase):
         self, component_name
     ):
         with patch('cdflow_commands.config.check_output') as check_output:
-            check_output.return_value = 'https://github.com/org/{}\n'.format(
-                component_name
+            check_output.return_value = bytes(
+                'https://github.com/org/{}\n'.format(
+                    component_name
+                ).encode('utf-8')
             )
             extraced_component_name = config.get_component_name(None)
 
