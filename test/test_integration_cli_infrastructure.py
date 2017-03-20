5fc041d868cdbc13e27417111fb43b682b0b30c2
grahamlyons <graham@grahamlyons.com>
2017-03-20 15:59:27 +0000

    Hook destroy action up for infrastructure projects
    This has a lot of common code with the ECS plugin so we can refactor
    that later.
    
    JIRA: PLAT-834

--- test/test_integration_cli_infrastructure.py
+++ test/test_integration_cli_infrastructure.py
@@ -4,10 +4,63 @@ from collections import namedtuple
 from io import TextIOWrapper
 
 from cdflow_commands import cli
-from mock import ANY, MagicMock, Mock, patch
+from mock import ANY, MagicMock, Mock, patch, mock_open
 
 
 BotoCreds = namedtuple('BotoCreds', ['access_key', 'secret_key', 'token'])
+
+
+class TestReleaseCLI(unittest.TestCase):
+
+    @patch('cdflow_commands.cli.rmtree')
+    @patch('cdflow_commands.cli.unlink')
+    @patch('cdflow_commands.cli.Session')
+    @patch('cdflow_commands.config.open', new_callable=mock_open, create=True)
+    @patch('cdflow_commands.config.check_output')
+    def test_release_is_a_no_op(
+        self, check_output, mock_open, Session_from_cli, _1, _2
+    ):
+        mock_metadata_file = MagicMock(spec=TextIOWrapper)
+        metadata = {
+            'TEAM': 'dummy-team',
+            'TYPE': 'infrastructure',
+            'REGION': 'eu-west-12',
+            'ACCOUNT_PREFIX': 'mmg'
+        }
+        mock_metadata_file.read.return_value = json.dumps(metadata)
+
+        mock_dev_file = MagicMock(spec=TextIOWrapper)
+        dev_config = {
+            'platform_config': {
+                'account_id': 123456789,
+            }
+        }
+        mock_dev_file.read.return_value = json.dumps(dev_config)
+
+        mock_prod_file = MagicMock(spec=TextIOWrapper)
+        prod_config = {
+            'platform_config': {
+                'account_id': 987654321,
+            }
+        }
+        mock_prod_file.read.return_value = json.dumps(prod_config)
+
+        mock_open.return_value.__enter__.side_effect = (
+            f for f in (mock_metadata_file, mock_dev_file, mock_prod_file)
+        )
+
+        mock_root_session = Mock()
+        mock_root_session.region_name = 'eu-west-12'
+        Session_from_cli.return_value = mock_root_session
+
+        with self.assertLogs('cdflow_commands.logger', level='INFO') as logs:
+            cli.run(['release'])
+
+        message = (
+            'INFO:cdflow_commands.logger:'
+            'Release takes no action on infrastructure type project'
+        )
+        assert message in logs.output
 
 
 class TestDeployCLI(unittest.TestCase):
@@ -187,3 +240,122 @@ class TestDeployCLI(unittest.TestCase):
                 'AWS_SESSION_TOKEN': self.aws_session_token
             }
         )
+
+
+class TestDestroyCLI(unittest.TestCase):
+
+    @patch('cdflow_commands.cli.rmtree')
+    @patch('cdflow_commands.cli.unlink')
+    @patch('cdflow_commands.plugins.infrastructure.S3BucketFactory')
+    @patch('cdflow_commands.plugins.base.os')
+    @patch('cdflow_commands.plugins.infrastructure.os')
+    @patch('cdflow_commands.cli.Session')
+    @patch('cdflow_commands.config.Session')
+    @patch('cdflow_commands.config.open', new_callable=mock_open, create=True)
+    @patch('cdflow_commands.plugins.base.check_call')
+    @patch('cdflow_commands.config.check_output')
+    def test_destroy_is_configured_and_run(
+        self, check_output, check_call, mock_open,
+        Session_from_config, Session_from_cli, mock_os_cli, mock_os_deploy, _,
+        unlink, rmtree
+    ):
+        # Given
+        mock_os_cli.environ = {
+            'JOB_NAME': 'dummy-job-name'
+        }
+        mock_os_deploy.environ = {}
+
+        mock_metadata_file = MagicMock(spec=TextIOWrapper)
+        metadata = {
+            'TEAM': 'dummy-team',
+            'TYPE': 'infrastructure',
+            'REGION': 'eu-west-12',
+            'ACCOUNT_PREFIX': 'mmg'
+        }
+        mock_metadata_file.read.return_value = json.dumps(metadata)
+
+        mock_dev_file = MagicMock(spec=TextIOWrapper)
+        dev_config = {
+            'platform_config': {
+                'account_id': 123456789,
+            }
+        }
+        mock_dev_file.read.return_value = json.dumps(dev_config)
+
+        mock_prod_file = MagicMock(spec=TextIOWrapper)
+        prod_config = {
+            'platform_config': {
+                'account_id': 987654321,
+            }
+        }
+        mock_prod_file.read.return_value = json.dumps(prod_config)
+
+        mock_open.return_value.__enter__.side_effect = (
+            f for f in (mock_metadata_file, mock_dev_file, mock_prod_file)
+        )
+
+        mock_sts_client = Mock()
+        mock_sts_client.assume_role.return_value = {
+            'Credentials': {
+                'AccessKeyId': 'dummy-access-key',
+                'SecretAccessKey': 'dummy-secret-key',
+                'SessionToken': 'dummy-session-token',
+            }
+        }
+
+        mock_root_session = Mock()
+        mock_root_session.client.return_value = mock_sts_client
+        mock_root_session.region_name = 'eu-west-12'
+        Session_from_cli.return_value = mock_root_session
+
+        aws_access_key_id = 'dummy-access-key-id'
+        aws_secret_access_key = 'dummy-secret-access-key'
+        aws_session_token = 'dummy-session-token'
+        mock_assumed_session = Mock()
+        mock_assumed_session.region_name = 'eu-west-12'
+        mock_assumed_session.get_credentials.return_value = BotoCreds(
+            aws_access_key_id,
+            aws_secret_access_key,
+            aws_session_token
+        )
+
+        Session_from_config.return_value = mock_assumed_session
+
+        component_name = 'dummy-component'
+
+        check_output.return_value = 'git@github.com:org/{}.git'.format(
+            component_name
+        ).encode('utf-8')
+
+        # When
+        cli.run(['destroy', 'aslive'])
+
+        # Then
+        check_call.assert_any_call(
+            [
+                'terragrunt', 'plan', '-destroy',
+                '-var', 'aws_region=eu-west-12',
+                '/cdflow/tf-destroy'
+            ],
+            env={
+                'AWS_ACCESS_KEY_ID': aws_access_key_id,
+                'AWS_SECRET_ACCESS_KEY': aws_secret_access_key,
+                'AWS_SESSION_TOKEN': aws_session_token
+            }
+        )
+
+        check_call.assert_any_call(
+            [
+                'terragrunt', 'destroy', '-force',
+                '-var', 'aws_region=eu-west-12',
+                '/cdflow/tf-destroy'
+            ],
+            env={
+                'AWS_ACCESS_KEY_ID': aws_access_key_id,
+                'AWS_SECRET_ACCESS_KEY': aws_secret_access_key,
+                'AWS_SESSION_TOKEN': aws_session_token
+            }
+        )
+
+        rmtree.assert_called_once_with('.terraform/')
+        unlink.assert_called_once_with('.terragrunt')
