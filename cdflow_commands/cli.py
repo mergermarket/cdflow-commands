54dceb2a2af38125f27f69289d2f55f3a66fde7e
Neil Haggerty <neil.haggerty@mergermarket.com>
2017-05-02 10:56:08 -0400

    Revert "Moving the deploy class to be used with lambda"
    This reverts commit 293e907e874bd7fe9bf3de281a6423d1a071173e.

--- cdflow_commands/cli.py
+++ cdflow_commands/cli.py
@@ -62,6 +62,7 @@ def _run(argv):
         global_config=global_config,
         root_session=root_session,
     )
+
     if args['release']:
         plugin.release()
     elif args['deploy']:
--- cdflow_commands/plugins/aws_lambda.py
+++ cdflow_commands/plugins/aws_lambda.py
@@ -1,60 +1,117 @@
+import json
 import os
+from os import path
+from subprocess import check_call
+from tempfile import NamedTemporaryFile
+from collections import namedtuple
 from zipfile import ZipFile
 from cdflow_commands.config import (
-    assume_role, get_role_session_name
+    assume_role, get_role_session_name, get_platform_config_path
 )
-from cdflow_commands.deploy import Deploy
+from cdflow_commands.plugins import Plugin
+from cdflow_commands.secrets import get_secrets
 
 
 def build_lambda_plugin(
     environment_name, component_name, version,
     metadata, global_config, root_session
 ):
-    return LambdaPlugin(
-        environment_name, component_name, version,
-        metadata, global_config, root_session
+    release_factory = build_release_factory(
+        global_config,
+        root_session,
+        component_name,
+        metadata,
+        version
     )
-
-
-class LambdaPlugin():
-    def __init__(
-        self,
+    deploy_factory = build_deploy_factory(
         environment_name,
         component_name,
         version,
         metadata,
         global_config,
         root_session
-    ):
-        self._environment_name = environment_name
-        self._component_name = component_name
-        self._version = version
-        self._metadata = metadata
-        self._global_config = global_config
-        self._boto_s3_client = assume_role(
+    )
+    return LambdaPlugin(
+        release_factory,
+        deploy_factory,
+        None
+    )
+
+
+def build_release_factory(
+    global_config, root_session, component_name, metadata, version
+):
+    def _release_factory():
+        boto_s3_client = assume_role(
             root_session,
             global_config.dev_account_id,
             get_role_session_name(os.environ)
         ).client('s3')
+        return Release(
+            global_config,
+            boto_s3_client,
+            component_name,
+            metadata,
+            version
+        )
+    return _release_factory
+
+
+def build_deploy_factory(
+    environment_name, component_name, version,
+    metadata, global_config, root_session
+):
+    def _deploy_factory():
+        is_prod = environment_name == 'live'
+        if is_prod:
+            account_id = global_config.prod_account_id
+        else:
+            account_id = global_config.dev_account_id
+
+        boto_session = assume_role(
+            root_session,
+            account_id,
+            get_role_session_name(os.environ)
+        )
+        deploy_config = DeployConfig(
+            team=metadata.team,
+            dev_account_id=global_config.dev_account_id,
+            platform_config_file=get_platform_config_path(
+                metadata.account_prefix,
+                metadata.aws_region,
+                is_prod
+            )
+        )
+        return Deploy(
+            boto_session,
+            component_name,
+            environment_name,
+            version,
+            deploy_config
+        )
+    return _deploy_factory
+
+
+class LambdaPlugin(Plugin):
+    def __init__(
+        self,
+        release_factory,
+        deploy_factory,
+        destroy_factory
+    ):
+        self.release_factory = release_factory
+        self.deploy_factory = deploy_factory
 
     def release(self):
-        release = Release(
-            self._global_config,
-            self._boto_s3_client,
-            self._component_name,
-            self._metadata,
-            self._version
-        )
+        release = self.release_factory()
         release.create()
 
     def deploy(self):
-        deploy = Deploy(
-            self._component_name,
-            self._environment_name,
-            self._version,
-            self._global_config
-        )
+        deploy = self.deploy_factory()
         deploy.run()
+
+    def destroy(self):
+        pass
 
 
 class Release():
@@ -101,3 +158,71 @@ class Release():
         )
 
         os.remove(zip.filename)
+
+
+DeployConfig = namedtuple('DeployConfig', [
+    'team',
+    'dev_account_id',
+    'platform_config_file',
+])
+
+
+class Deploy(object):
+
+    def __init__(
+        self, boto_session, component_name, environment_name,
+        version, deploy_config
+    ):
+        self._boto_session = boto_session
+        self._aws_region = boto_session.region_name
+        self._component_name = component_name
+        self._environment_name = environment_name
+        self._version = version
+        self._team = deploy_config.team
+        self._dev_account_id = deploy_config.dev_account_id
+        self._platform_config_file = deploy_config.platform_config_file
+
+    def _terraform_parameters(self, secrets_file):
+        parameters = [
+            '-var', 'component={}'.format(self._component_name),
+            '-var', 'env={}'.format(self._environment_name),
+            '-var', 'aws_region={}'.format(self._aws_region),
+            '-var', 'team={}'.format(self._team),
+            '-var', 'version={}'.format(self._version),
+            '-var-file', self._platform_config_file,
+            '-var-file', secrets_file
+        ]
+        config_file = 'config/{}.json'.format(self._environment_name)
+        if path.exists(config_file):
+            parameters += ['-var-file', config_file]
+        return parameters + ['infra']
+
+    def run(self):
+        check_call(['terraform', 'get', 'infra'])
+
+        credentials = self._boto_session.get_credentials()
+        env = os.environ.copy()
+        env.update({
+            'AWS_ACCESS_KEY_ID': credentials.access_key,
+            'AWS_SECRET_ACCESS_KEY': credentials.secret_key,
+            'AWS_SESSION_TOKEN': credentials.token
+        })
+
+        with NamedTemporaryFile() as f:
+            secrets = get_secrets(
+                self._environment_name,
+                self._team,
+                self._component_name,
+                self._boto_session
+            )
+            f.write(json.dumps({'secrets': secrets}).encode('utf-8'))
+            f.flush()
+            parameters = self._terraform_parameters(f.name)
+            check_call(
+                ['terraform', 'plan'] + parameters,
+                env=env
+            )
+            check_call(
+                ['terraform', 'apply'] + parameters,
+                env=env
+            )
