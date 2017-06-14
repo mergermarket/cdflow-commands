8e43457b2399a7fd1adcffff34b1f9bd5db2c3b4
grahamlyons <graham@grahamlyons.com>
2017-06-14 10:54:28 +0100

    Remove message, which was not added anyway

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 for REQUIREMENTS_IN in requirements test_requirements
 do
 docker run --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" python:3 \
-    sh -c "pip install -r ${REQUIREMENTS_IN}.in && echo Automatically generated: ${REQUIREMENTS_IN} > ${REQUIREMENTS_IN}.txt && pip freeze > ${REQUIREMENTS_IN}.txt"
+    sh -c "pip install -r ${REQUIREMENTS_IN}.in && pip freeze > ${REQUIREMENTS_IN}.txt"
 done
