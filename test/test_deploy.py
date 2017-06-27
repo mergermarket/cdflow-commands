c47de1cc79e7ee460d77c8a9a649a571fd4cbcc7
grahamlyons <graham@grahamlyons.com>
2017-06-27 15:33:59 +0100

    Add support for global `all.json` config file
    JIRA: PLAT-945

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -56,7 +56,9 @@ class TestDeploy(unittest.TestCase):
         )
 
         with ExitStack() as stack:
-            stack.enter_context(patch('cdflow_commands.deploy.path'))
+            path_exists = stack.enter_context(
+                patch('cdflow_commands.deploy.path.exists')
+            )
             check_call = stack.enter_context(
                 patch('cdflow_commands.deploy.check_call')
             )
@@ -82,6 +84,12 @@ class TestDeploy(unittest.TestCase):
 
             dummy_plugin = Mock()
             dummy_plugin.parameters.return_value = []
+
+            def mock_path_exists(path):
+                if path == 'config/{}.json'.format(environment):
+                    return True
+
+            path_exists.side_effect = mock_path_exists
 
             deploy.run(dummy_plugin)
 
@@ -146,7 +154,7 @@ class TestDeploy(unittest.TestCase):
         )
 
         with ExitStack() as stack:
-            stack.enter_context(patch('cdflow_commands.deploy.path'))
+            stack.enter_context(patch('cdflow_commands.deploy.path.exists'))
             stack.enter_context(
                 patch('cdflow_commands.deploy.NamedTemporaryFile')
             )
@@ -220,7 +228,9 @@ class TestDeploy(unittest.TestCase):
         )
 
         with ExitStack() as stack:
-            stack.enter_context(patch('cdflow_commands.deploy.path'))
+            path_exists = stack.enter_context(
+                patch('cdflow_commands.deploy.path.exists')
+            )
             NamedTemporaryFile = stack.enter_context(
                 patch('cdflow_commands.deploy.NamedTemporaryFile')
             )
@@ -241,6 +251,12 @@ class TestDeploy(unittest.TestCase):
 
             secret_file_path = NamedTemporaryFile.return_value.__enter__\
                 .return_value
+
+            def mock_path_exists(path):
+                if path == 'config/{}.json'.format(environment):
+                    return True
+
+            path_exists.side_effect = mock_path_exists
 
             mock_os.environ = {}
 
@@ -263,6 +279,198 @@ class TestDeploy(unittest.TestCase):
                     '-var-file', secret_file_path,
                     '-out', 'plan-{}'.format(utcnow),
                     '-var-file', 'config/{}.json'.format(environment),
+                ],
+                cwd=release_path,
+                env={
+                    'AWS_ACCESS_KEY_ID': credentials.access_key,
+                    'AWS_SECRET_ACCESS_KEY': credentials.secret_key,
+                    'AWS_SESSION_TOKEN': credentials.token,
+                }
+            )
+
+    @given(fixed_dictionaries({
+        'component': text(),
+        'version': text(),
+        'environment': text(),
+        'team': text(),
+        'release_path': text(),
+        'account': text(),
+        'utcnow': text(alphabet=digits),
+        'access_key': text(),
+        'secret_key': text(),
+        'token': text(),
+    }))
+    def test_environment_config_not_added_if_not_present(self, fixtures):
+        component = fixtures['component']
+        version = fixtures['version']
+        environment = fixtures['environment']
+        team = fixtures['team']
+        release_path = fixtures['release_path']
+        account = fixtures['account']
+        utcnow = fixtures['utcnow']
+        access_key = fixtures['access_key']
+        secret_key = fixtures['secret_key']
+        token = fixtures['token']
+
+        account_scheme = MagicMock(spec=AccountScheme)
+        account_scheme.account_for_environment.return_value = account
+
+        boto_session = Mock()
+        boto_session.region_name = 'us-north-4'
+        credentials = BotoCredentials(access_key, secret_key, token)
+        boto_session.get_credentials.return_value = credentials
+
+        deploy = Deploy(
+            component, version, environment, team,
+            release_path, account_scheme, boto_session,
+        )
+
+        with ExitStack() as stack:
+            path_exists = stack.enter_context(
+                patch('cdflow_commands.deploy.path.exists')
+            )
+            check_call = stack.enter_context(
+                patch('cdflow_commands.deploy.check_call')
+            )
+            NamedTemporaryFile = stack.enter_context(
+                patch('cdflow_commands.deploy.NamedTemporaryFile')
+            )
+            mock_os = stack.enter_context(patch('cdflow_commands.deploy.os'))
+            get_secrets = stack.enter_context(
+                patch('cdflow_commands.deploy.get_secrets')
+            )
+            datetime = stack.enter_context(
+                patch('cdflow_commands.deploy.datetime')
+            )
+
+            datetime.utcnow.return_value.strftime.return_value = utcnow
+
+            get_secrets.return_value = {}
+
+            secret_file_path = NamedTemporaryFile.return_value.__enter__\
+                .return_value
+
+            mock_os.environ = {}
+
+            dummy_plugin = Mock()
+            dummy_plugin.parameters.return_value = []
+
+            path_exists.return_value = False
+
+            deploy.run(dummy_plugin)
+
+            check_call.assert_any_call(
+                [
+                    'terraform', 'plan', 'infra',
+                    '-var', 'component={}'.format(component),
+                    '-var', 'env={}'.format(environment),
+                    '-var', 'aws_region={}'.format(boto_session.region_name),
+                    '-var', 'team={}'.format(team),
+                    '-var', 'version={}'.format(version),
+                    '-var-file', 'platform-config/{}/{}.json'.format(
+                        account, boto_session.region_name
+                    ),
+                    '-var-file', secret_file_path,
+                    '-out', 'plan-{}'.format(utcnow),
+                ],
+                cwd=release_path,
+                env={
+                    'AWS_ACCESS_KEY_ID': credentials.access_key,
+                    'AWS_SECRET_ACCESS_KEY': credentials.secret_key,
+                    'AWS_SESSION_TOKEN': credentials.token,
+                }
+            )
+
+    @given(fixed_dictionaries({
+        'component': text(),
+        'version': text(),
+        'environment': text(),
+        'team': text(),
+        'release_path': text(),
+        'account': text(),
+        'utcnow': text(alphabet=digits),
+        'access_key': text(),
+        'secret_key': text(),
+        'token': text(),
+    }))
+    def test_global_environment_config_added_if_present(self, fixtures):
+        component = fixtures['component']
+        version = fixtures['version']
+        environment = fixtures['environment']
+        team = fixtures['team']
+        release_path = fixtures['release_path']
+        account = fixtures['account']
+        utcnow = fixtures['utcnow']
+        access_key = fixtures['access_key']
+        secret_key = fixtures['secret_key']
+        token = fixtures['token']
+
+        account_scheme = MagicMock(spec=AccountScheme)
+        account_scheme.account_for_environment.return_value = account
+
+        boto_session = Mock()
+        boto_session.region_name = 'us-north-4'
+        credentials = BotoCredentials(access_key, secret_key, token)
+        boto_session.get_credentials.return_value = credentials
+
+        deploy = Deploy(
+            component, version, environment, team,
+            release_path, account_scheme, boto_session,
+        )
+
+        with ExitStack() as stack:
+            path_exists = stack.enter_context(
+                patch('cdflow_commands.deploy.path.exists')
+            )
+            check_call = stack.enter_context(
+                patch('cdflow_commands.deploy.check_call')
+            )
+            NamedTemporaryFile = stack.enter_context(
+                patch('cdflow_commands.deploy.NamedTemporaryFile')
+            )
+            mock_os = stack.enter_context(patch('cdflow_commands.deploy.os'))
+            get_secrets = stack.enter_context(
+                patch('cdflow_commands.deploy.get_secrets')
+            )
+            datetime = stack.enter_context(
+                patch('cdflow_commands.deploy.datetime')
+            )
+
+            datetime.utcnow.return_value.strftime.return_value = utcnow
+
+            get_secrets.return_value = {}
+
+            secret_file_path = NamedTemporaryFile.return_value.__enter__\
+                .return_value
+
+            mock_os.environ = {}
+
+            dummy_plugin = Mock()
+            dummy_plugin.parameters.return_value = []
+
+            def mock_path_exists(path):
+                if path == 'config/all.json':
+                    return True
+                else:
+                    return False
+            path_exists.side_effect = mock_path_exists
+
+            deploy.run(dummy_plugin)
+
+            check_call.assert_any_call(
+                [
+                    'terraform', 'plan', 'infra',
+                    '-var', 'component={}'.format(component),
+                    '-var', 'env={}'.format(environment),
+                    '-var', 'aws_region={}'.format(boto_session.region_name),
+                    '-var', 'team={}'.format(team),
+                    '-var', 'version={}'.format(version),
+                    '-var-file', 'platform-config/{}/{}.json'.format(
+                        account, boto_session.region_name
+                    ),
+                    '-var-file', secret_file_path,
+                    '-out', 'plan-{}'.format(utcnow),
+                    '-var-file', 'config/all.json',
                 ],
                 cwd=release_path,
                 env={
