921cf6e1f8a0dad3d545f16b098d32bed5af8576
Bart Swedrowski <bart@timedout.org>
2017-10-23 12:18:06 +0100

    WIP

--- cdflow_commands/process.py
+++ cdflow_commands/process.py
@@ -1,10 +1,19 @@
 from cdflow_commands.exceptions import UserFacingError
 
-from subprocess import CalledProcessError, check_call as _check_call
+from subprocess import check_call as _check_call
+from subprocess import Popen as _popen
+from subprocess import CalledProcessError
 
 
 def check_call(*args, **kwargs):
     try:
         _check_call(*args, **kwargs)
+    except CalledProcessError as e:
+        raise UserFacingError(e)
+
+
+def popen(*args, **kwargs):
+    try:
+        _popen(*args, **kwargs)
     except CalledProcessError as e:
         raise UserFacingError(e)
