1dd3ee94ef90940b801919d8bdffdead0862bc6e
grahamlyons <graham@grahamlyons.com>
2018-09-03 11:53:56 +0100

    Select an existing workspace if there is one
    Calling `new` on an existing workspace causes Terraform to fail.

--- cdflow_commands/process.py
+++ cdflow_commands/process.py
@@ -1,11 +1,18 @@
 from cdflow_commands.exceptions import UserFacingError
 
-from subprocess import check_call as _check_call
+from subprocess import check_call as _check_call, check_output as _check_output
 from subprocess import CalledProcessError
 
 
 def check_call(*args, **kwargs):
     try:
         _check_call(*args, **kwargs)
+    except CalledProcessError as e:
+        raise UserFacingError(e)
+
+
+def check_output(*args, **kwargs):
+    try:
+        _check_output(*args, **kwargs)
     except CalledProcessError as e:
         raise UserFacingError(e)
