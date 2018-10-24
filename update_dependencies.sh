f1488e83588395356e73f68c44113599cafc81fe
grahamlyons <graham@grahamlyons.com>
2018-10-24 15:00:36 +0100

    Add Pipfiles and remove the requirements files

--- update_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-for REQUIREMENTS_IN in requirements test_requirements
-do
-docker run --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" python:3 \
-    sh -c "pip install -r ${REQUIREMENTS_IN}.in && pip freeze > ${REQUIREMENTS_IN}.txt"
-done
