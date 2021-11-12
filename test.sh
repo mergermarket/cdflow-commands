4e07bb095160c3ec00de8399f00145110231cd07
Ian Jones <ian.jones@iongroup.com>
2021-11-12 12:20:22 -0500

    Removed '-n' argument from test run and renamed .travis.yml

--- test.sh
+++ test.sh
@@ -10,7 +10,6 @@ docker run \
     -i $(tty -s && echo -t) \
     $(tty -s && echo -v $(pwd)/.hypothesis/:/usr/src/app/.hypothesis/) \
     cdflow-commands.test py.test \
-    	-n auto \
         --cov=. \
         --cov-report term-missing \
         --tb=short \
